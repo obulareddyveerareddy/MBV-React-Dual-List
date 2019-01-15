14a308e2309ad84927d513523e6caa37034ae9cf
Veera Reddy Obula <veerareddy.obula@gmail.com>
2019-01-15 21:38:49 +0530

    Fixed Remove Group Issue

--- src/react-dual-list/react-dual-list.js
+++ src/react-dual-list/react-dual-list.js
@@ -8,89 +8,105 @@ export default class ReactDualList extends React.Component {
     this.state = {
       rightListOptions: [
         {
-            label: 'Earth',
-            options: [
-                { value: 'luna', label: 'Moon' },
-            ],
+          label: "Earth",
+          options: [{ value: "luna", label: "Moon" }]
         },
         {
-            label: 'Mars',
-            options: [
-                { value: 'phobos', label: 'Phobos' },
-                { value: 'deimos', label: 'Deimos' },
-            ],
+          label: "Mars",
+          options: [
+            { value: "phobos", label: "Phobos" },
+            { value: "deimos", label: "Deimos" }
+          ]
         },
         {
-            label: 'Jupiter',
-            options: [
-                { value: 'io', label: 'Io' },
-                { value: 'europa', label: 'Europa' },
-                { value: 'ganymede', label: 'Ganymede' },
-                { value: 'callisto', label: 'Callisto' },
-            ],
-        },
-    ],
+          label: "Jupiter",
+          options: [
+            { value: "io", label: "Io" },
+            { value: "europa", label: "Europa" },
+            { value: "ganymede", label: "Ganymede" },
+            { value: "callisto", label: "Callisto" }
+          ]
+        }
+      ],
       leftListOptions: []
     };
   }
   onClickHandler = (event, entity, direction, childEntity) => {
     const { leftListOptions, rightListOptions } = this.state;
-    var clonedEntity = _.cloneDeep(entity)
+    var clonedEntity = _.cloneDeep(entity);
     if (direction === "right") {
-      if(!childEntity || (entity.options && entity.options.length === 1)){
+      if (!childEntity || (entity.options && entity.options.length === 1)) {
         _.remove(rightListOptions, function(item) {
           return item.value === entity.value && item.label === entity.label;
         });
-      }else {
-        clonedEntity = _.cloneDeep(entity)
-        _.remove(clonedEntity.options, function(item){
-          return item.value !== childEntity.value && item.label !== childEntity.label;
-        })
-        _.forEach(rightListOptions, function (item) {
-          if(item.label === entity.label){
-            _.remove(item.options, function(item){
-              return item.value === childEntity.value && item.label === childEntity.label;
-            })
+      } else {
+        clonedEntity = _.cloneDeep(entity);
+        _.remove(clonedEntity.options, function(item) {
+          return (
+            item.value !== childEntity.value && item.label !== childEntity.label
+          );
+        });
+        _.forEach(rightListOptions, function(item) {
+          if (item.label === entity.label) {
+            _.remove(item.options, function(item) {
+              return (
+                item.value === childEntity.value &&
+                item.label === childEntity.label
+              );
+            });
           }
         });
       }
-      if(_.find(leftListOptions, {label:entity.label})){
-        _.forEach(leftListOptions, function(item){
-          if(item.label === entity.label){
-            item.options.push(childEntity)
+      if (_.find(leftListOptions, { label: entity.label })) {
+        _.forEach(leftListOptions, function(item) {
+          if (item.label === entity.label) {
+            if (!childEntity) {
+              item.options = _.concat(item.options, entity.options);
+            } else {
+              item.options.push(childEntity);
+            }
           }
-        })
-      }else {
-        leftListOptions.push(clonedEntity)
+        });
+      } else {
+        leftListOptions.push(clonedEntity);
       }
-      
+
       this.setState({ leftListOptions, rightListOptions });
     } else if (direction === "left") {
-      if(!childEntity || (entity.options && entity.options.length === 1)){
+      if (!childEntity || (entity.options && entity.options.length === 1)) {
         _.remove(leftListOptions, function(item) {
           return item.value === entity.value && item.label === entity.label;
         });
-      }else {
-        clonedEntity = _.cloneDeep(entity)
-        _.remove(clonedEntity.options, function(item){
-          return item.value !== childEntity.value && item.label !== childEntity.label;
-        })
-        _.forEach(leftListOptions, function (item) {
-          if(item.label === entity.label){
-            _.remove(item.options, function (item) {
-              return item.value === childEntity.value && item.label === childEntity.label;
-            })
+      } else {
+        clonedEntity = _.cloneDeep(entity);
+        _.remove(clonedEntity.options, function(item) {
+          return (
+            item.value !== childEntity.value && item.label !== childEntity.label
+          );
+        });
+        _.forEach(leftListOptions, function(item) {
+          if (item.label === entity.label) {
+            _.remove(item.options, function(item) {
+              return (
+                item.value === childEntity.value &&
+                item.label === childEntity.label
+              );
+            });
           }
         });
       }
-      if(_.find(rightListOptions, {label:entity.label})){
-        _.forEach(rightListOptions, function(item){
-          if(item.label === entity.label){
-            item.options.push(childEntity)
+      if (_.find(rightListOptions, { label: entity.label })) {
+        _.forEach(rightListOptions, function(item) {
+          if (item.label === entity.label) {
+            if (!childEntity) {
+              item.options = _.concat(item.options, entity.options);
+            } else {
+              item.options.push(childEntity);
+            }
           }
-        })
-      }else {
-        rightListOptions.push(clonedEntity)
+        });
+      } else {
+        rightListOptions.push(clonedEntity);
       }
       this.setState({ rightListOptions, leftListOptions });
     }
